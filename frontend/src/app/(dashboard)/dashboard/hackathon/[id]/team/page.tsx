e48ae0a1983adeb0ee422e678b7c1be162a9c9f9
faizm10 <faizmustansar10@gmail.com>
2025-07-26 20:29:56 -0400

    frontend: moved teampage to components folder

--- frontend/src/app/(dashboard)/dashboard/hackathon/[id]/team/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/hackathon/[id]/team/page.tsx
@@ -1,4 +1,4 @@
-import TeamPage from './TeamPage'
+import TeamPage from '@/components/TeamPage'
 
 export default async function HackathonTeamPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
